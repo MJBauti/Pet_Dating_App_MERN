cdb9bcf1c2231895cb11ed95567ea6b5f256692b
Andrew Choi <andrewsungwoochoi@gmail.com>
2022-12-17 15:48:30 -0800

    debug

--- server/server.js
+++ server/server.js
@@ -20,9 +20,12 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../../client/build')));
+}
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
 // Stripe 
