3c04fcc7f20d3d669c22ff6e25f282dbbc7b1273
Andrew Choi <andrewsungwoochoi@gmail.com>
2022-12-17 15:47:03 -0800

    debug

--- server/server.js
+++ server/server.js
@@ -20,9 +20,6 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
