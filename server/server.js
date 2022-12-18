1648872d2ecb36c948e6a1aef3ff461ace86eae4
Andrew Choi <andrewsungwoochoi@gmail.com>
2022-12-17 16:38:56 -0800

    debug

--- server/server.js
+++ server/server.js
@@ -52,8 +52,8 @@ app.post('/create-checkout-session', async (req, res) => {
         },
       ],
       mode: 'payment',
-      success_url: `http://localhost:3001?success=true`,
-      cancel_url: `http://localhost:3001?canceled=true`,
+      success_url: `./?success=true`,
+      cancel_url: `./?canceled=true`,
     });
 
 
