cb91aeffc0b9c525e6c80f6612cac8e35db7275f
Andrew Choi <andrewsungwoochoi@gmail.com>
2022-12-19 11:37:45 -0800

    debugging for heroku

--- client/src/components/PaymentSuccess/PaymentSuccess.js
+++ client/src/components/PaymentSuccess/PaymentSuccess.js
@@ -1,39 +1,39 @@
-import React, { useState } from 'react';
-import Stripe from '@stripe/react-stripe-js';
+// import React, { useState } from 'react';
+// import Stripe from '@stripe/react-stripe-js';
 
-export const PaymentSuccess = () => {
-  const stripe = new Stripe(process.env.STRIPE_PUBLISHABLE_KEY);
-  const [paymentSuccess, setPaymentSuccess] = useState(false);
+// export const PaymentSuccess = () => {
+//   const stripe = new Stripe(process.env.STRIPE_PUBLISHABLE_KEY);
+//   const [paymentSuccess, setPaymentSuccess] = useState(false);
 
-  const handlePaymentSubmit = async (req, res) => {
-    try {
-      const paymentIntent = await stripe.paymentIntent.create({
-        amount: parseInt(req.body.price)*100,
-        currency: 'usd',
+//   const handlePaymentSubmit = async (req, res) => {
+//     try {
+//       const paymentIntent = await stripe.paymentIntent.create({
+//         amount: parseInt(req.body.price)*100,
+//         currency: 'usd',
           
-      });
-      paymentIntent(true);
-      setPaymentSuccess(true);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+//       });
+//       paymentIntent(true);
+//       setPaymentSuccess(true);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
 
-  if (paymentSuccess) {
-    return (
-      <div>
-        <h1>Payment Successful</h1>
-        <p>Thank you for your payment!</p>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <button onClick={handlePaymentSubmit}>Submit Payment</button>
-      </div>
-    );
-  }
-};
+//   if (paymentSuccess) {
+//     return (
+//       <div>
+//         <h1>Payment Successful</h1>
+//         <p>Thank you for your payment!</p>
+//       </div>
+//     );
+//   } else {
+//     return (
+//       <div>
+//         <button onClick={handlePaymentSubmit}>Submit Payment</button>
+//       </div>
+//     );
+//   }
+// };
 
-export default PaymentSuccess;
+// export default PaymentSuccess;
 
